1adc57e6b5e3e1639799be7fdbc135f51a8ad68e
powellor <oliver.powell@kaust.edu.sa>
2024-01-25 16:47:28 +0300

    formatting

--- src/blast_2_bed.py
+++ src/blast_2_bed.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-def BLAST2BED9(input: str, output: str, locus_size: int, exon_count: int, q_cov_threshold: float) -> None:
+
+def BLAST2BED9(
+    input: str, output: str, locus_size: int, exon_count: int, q_cov_threshold: float
+) -> None:
     cds_blast_data = pd.read_csv(
         input,
         sep="\t",
@@ -24,7 +27,9 @@ def BLAST2BED9(input: str, output: str, locus_size: int, exon_count: int, q_cov_
 
     # Establish strand orientation and query coverage of BLAST hits
     cds_blast_data["orientation"] = cds_blast_data.sstart < cds_blast_data.send
-    cds_blast_data["strand"] = cds_blast_data.orientation.map(lambda x: "+" if x is True else "-")
+    cds_blast_data["strand"] = cds_blast_data.orientation.map(
+        lambda x: "+" if x is True else "-"
+    )
     cds_blast_data["qcov"] = round(cds_blast_data.length / cds_blast_data.qlen, 2)
 
     # Correctly order start/end of BLAST hits for BED
@@ -42,34 +47,51 @@ def BLAST2BED9(input: str, output: str, locus_size: int, exon_count: int, q_cov_
 
     # Remove any formatting from BLASTDB from sequence IDs
     if any(cds_blast_data.sseqid.str.contains("|")):
-        cds_blast_data["chromosome"] = cds_blast_data.sseqid.map(lambda x: x.split("|")[1])
+        cds_blast_data["chromosome"]: str = cds_blast_data.sseqid.map(   # type: ignore
+            lambda x: x.split("|")[1]
+        )
 
     else:
         cds_blast_data["chromosome"] = cds_blast_data.sseqid
 
     # Fill columns 2-9
-    bed9["chrom"]: str = cds_blast_data.chromosome
+    bed9["chrom"]: str = cds_blast_data.chromosome  # type: ignore
     bed9["chromStart"]: int = cds_blast_data.sstart  # type: ignore
     bed9["chromEnd"]: int = cds_blast_data.send  # type: ignore
-    bed9["name"]: str = [f"exon_{i}" for i in cds_blast_data.index]
-    bed9["score"]: float = cds_blast_data.qcov
-    bed9["strand"]: str = cds_blast_data.strand
+    bed9["name"]: str = [f"exon_{i}" for i in cds_blast_data.index]  # type: ignore
+    bed9["score"]: float = cds_blast_data.qcov  # type: ignore
+    bed9["strand"]: str = cds_blast_data.strand  # type: ignore
     bed9["thickStart"]: int = cds_blast_data.sstart  # type: ignore
     bed9["thickEnd"]: int = cds_blast_data.send  # type: ignore
-    bed9["itemRgb"]: str = "145,30,180"
+    bed9["itemRgb"]: str = "145,30,180"  # type: ignore
 
     # Label gene loci according to parameters and add to bed file
-    # This provides confidence in the absence of annotations 
+    # This provides confidence in the absence of annotations
     gene_locus = 0
 
-    for window in  cds_blast_data.sort_values(["chromosome","strand"]).rolling(exon_count):
-        if len(window) > exon_count-1 and all(window.chromosome.unique()) and all(window.strand.unique()):
-            locus_qcov = round(window.qcov.sum(),2)
+    for window in cds_blast_data.sort_values(["chromosome", "strand"]).rolling(
+        exon_count
+    ):
+        if (
+            len(window) > exon_count - 1
+            and all(window.chromosome.unique())  # type: ignore
+            and all(window.strand.unique())
+        ):
+            locus_qcov = round(window.qcov.sum(), 2)
             size = window.send.max() - window.sstart.min()
 
-            if locus_qcov > q_cov_threshold and size < locus_size *1.5:
-                
-                bed9.loc[len(bed9.index)] = [window.chromosome.unique()[0], window.sstart.min(), window.send.max(), f"locus_{gene_locus}", locus_qcov, window.strand.unique()[0], window.sstart.min(), window.send.max(), "0,255,0"]
+            if locus_qcov > q_cov_threshold and size < locus_size * 1.5:
+                bed9.loc[len(bed9.index)] = [
+                    window.chromosome.unique()[0],
+                    window.sstart.min(),
+                    window.send.max(),
+                    f"locus_{gene_locus}",
+                    locus_qcov,
+                    window.strand.unique()[0],
+                    window.sstart.min(),
+                    window.send.max(),
+                    "0,255,0",
+                ]
                 gene_locus += 1
 
     # Save output to bed file (formatted as tsv)
@@ -91,9 +113,7 @@ def BLAST2BED9(input: str, output: str, locus_size: int, exon_count: int, q_cov_
         ],
     )
 
-
 if __name__ == "__main__":
-
     NLR_EXONS = 3
     NLR_LOCUS = 6000
     NLR_QCOV = 0.8
@@ -102,20 +122,19 @@ if __name__ == "__main__":
     KINASE_LOCUS = 10000
     KINASE_QCOV = 0.8
 
-
     BLAST2BED9(
         "~/Documents/projects/sr62_homeologues/analysis/blast_annotation_mapping/test_data/chinese_spring/Sr62_cds.blastn",
         "Sr62_cds.bed",
         exon_count=KINASE_EXONS,
         q_cov_threshold=KINASE_QCOV,
-        locus_size=KINASE_LOCUS
+        locus_size=KINASE_LOCUS,
     )
     BLAST2BED9(
         "~/Documents/projects/sr62_homeologues/analysis/blast_annotation_mapping/test_data/chinese_spring/Sr62_cds_e0001.blastn",
         "Sr62_cds_refined.bed",
-            exon_count=KINASE_EXONS,
+        exon_count=KINASE_EXONS,
         q_cov_threshold=KINASE_QCOV,
-        locus_size=KINASE_LOCUS
+        locus_size=KINASE_LOCUS,
     )
 
     BLAST2BED9(
@@ -123,7 +142,7 @@ if __name__ == "__main__":
         "SrNLR_cds.bed",
         exon_count=NLR_EXONS,
         locus_size=NLR_LOCUS,
-        q_cov_threshold=NLR_QCOV
+        q_cov_threshold=NLR_QCOV,
     )
 
     BLAST2BED9(
@@ -131,7 +150,7 @@ if __name__ == "__main__":
         "SrNLR_cds_refined.bed",
         exon_count=NLR_EXONS,
         locus_size=NLR_LOCUS,
-        q_cov_threshold=NLR_QCOV
+        q_cov_threshold=NLR_QCOV,
     )
 
     BLAST2BED9(
@@ -139,7 +158,7 @@ if __name__ == "__main__":
         "SrNLR_cds_refined.mega.bed",
         exon_count=NLR_EXONS,
         locus_size=NLR_LOCUS,
-        q_cov_threshold=NLR_QCOV
+        q_cov_threshold=NLR_QCOV,
     )
 
     BLAST2BED9(
@@ -147,5 +166,5 @@ if __name__ == "__main__":
         "Sr62_cds_refined.mega.bed",
         exon_count=KINASE_EXONS,
         q_cov_threshold=KINASE_QCOV,
-        locus_size=KINASE_LOCUS
+        locus_size=KINASE_LOCUS,
     )
