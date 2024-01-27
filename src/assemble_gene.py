6a68bc5348aeb4cf4d4aac4a7eff7caffe406230
powellor <oliver.powell@kaust.edu.sa>
2024-01-27 15:22:15 +0300

    Extract exon sequences now uses subprocess to allow for error handling from BLAST

--- src/assemble_gene.py
+++ src/assemble_gene.py
@@ -1,6 +1,8 @@
-import os
 import tempfile
 import pandas as pd
+import subprocess
+import sys
+import os
 from Bio import SeqIO
 
 
@@ -25,24 +27,35 @@ class GeneAssembler:
 
         self.exon_list: list[str] = EXON_LIST
 
-        self.exon_data = self.bed[self.bed.isin(self.exon_list)]
-
-        self.sequences_extracted = False
+        self.exon_data = self.bed[self.bed.name.isin(self.exon_list)]
 
         self.blastdb: str = BLASTDB_PATH
+
+        self.sequences_extracted = False
 
         self.batch_entry_file = tempfile.NamedTemporaryFile(delete=False)
 
         self.exon_sequence_file = tempfile.NamedTemporaryFile(delete=False)
 
+        print(self.exon_sequence_file.name)
+
     def extract_exon_sequences(self) -> None:
+        print(self.bed)
+        print(self.exon_data)
         with open(self.batch_entry_file.name, "w+") as file:
             for index, row in self.exon_data.iterrows():
                 file.write(f"{row.chrom} {row.chromStart}-{row.chromEnd}\n")
 
-        blastcmd = f"blastdbcmd -db {self.blastdb} -entry_batch tmp_entry_batch.txt -out {self.exon_sequence_file.name} -outfmt %f"
+        try:
+            
+            subprocess.run(["blastdbcmd", "-db", f"{self.blastdb}", "-entry_batch", f"{self.batch_entry_file.name}", "-out", f"{self.exon_sequence_file.name}", "-outfmt", "%f"], check=True)
+            self.sequences_extracted = True
 
-        os.system(blastcmd)
+        except (subprocess.CalledProcessError, FileNotFoundError) as error:
+            print(error)
+            sys.exit(1)
+
+    
 
 
 def fetch_exons(BED_FILE, EXON_LIST, BLASTDB_PATH) -> None:
@@ -62,9 +75,7 @@ def fetch_exons(BED_FILE, EXON_LIST, BLASTDB_PATH) -> None:
             "itemRgb",
         ],
     )
-    print(bed)
     bed = bed[bed.name.isin(EXON_LIST)]
-    print(bed)
     with open("tmp_entry_batch.txt", "w+") as file:
         for index, row in bed.iterrows():
             file.write(f"{row.chrom} {row.chromStart}-{row.chromEnd}\n")
@@ -99,8 +110,11 @@ def predict_gene() -> None:
 if __name__ == "__main__":
     CS_BLASTDB_PATH = "/home/powellor/Documents/projects/sr62_homeologues/data/wheat_genomes/chinese_spring/cs_blastdb/cs_blastdb"
     LONG_BLASTDB_PATH = "/home/powellor/Documents/projects/sr62_homeologues/data/sitopsis_genomes/longissima_blastdb/longissima_blastdb"
-
     BED_FILE = "/home/powellor/Documents/projects/sr62_homeologues/scripts/SABAT_pipeline/SABAT/src/longissima_nlr_cds_refined.mega.bed"
     EXON_LIST = ["exon_42", "exon_43", "exon_44"]
+
     fetch_exons(BED_FILE=BED_FILE, EXON_LIST=EXON_LIST, BLASTDB_PATH=LONG_BLASTDB_PATH)
     predict_gene()
+
+    gene = GeneAssembler(BED_FILE=BED_FILE, BLASTDB_PATH=LONG_BLASTDB_PATH, EXON_LIST=EXON_LIST)
+    gene.extract_exon_sequences()
