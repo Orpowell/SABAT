c0852410b36cef2f15a8fa4d83bfaeb861a0bd4f
powellor <oliver.powell@kaust.edu.sa>
2024-01-29 15:09:36 +0300

    removed old original functions for fetch exons and predict_gene

--- src/assemble_gene.py
+++ src/assemble_gene.py
@@ -5,6 +5,7 @@ import sys
 import os
 import warnings
 from Bio import SeqIO
+from Bio.Seq import Seq
 
 warnings.filterwarnings("ignore")
 
@@ -100,57 +101,9 @@ class GeneAssembler:
     def run(self) -> None:
 
         self.extract_exon_sequences()
-        self.predict_gene()
+        #self.predict_gene()
+        self.better_predict_gene()
         self.generate_statistics()
-
-
-def fetch_exons(BED_FILE, EXON_LIST, BLASTDB_PATH) -> None:
-    bed = pd.read_csv(
-        BED_FILE,
-        sep="\t",
-        header=None,
-        names=[
-            "chrom",
-            "chromStart",
-            "chromEnd",
-            "name",
-            "score",
-            "strand",
-            "thickStart",
-            "thickEnd",
-            "itemRgb",
-        ],
-    )
-    bed = bed[bed.name.isin(EXON_LIST)]
-    with open("tmp_entry_batch.txt", "w+") as file:
-        for index, row in bed.iterrows():
-            file.write(f"{row.chrom} {row.chromStart}-{row.chromEnd}\n")
-
-    blastcmd = f"blastdbcmd -db {BLASTDB_PATH} -entry_batch tmp_entry_batch.txt -out exon_sequences.fa -outfmt %f"
-
-    os.system(blastcmd)
-
-
-def predict_gene() -> None:
-    with open("exon_sequences.fa") as handle:
-        seqs = [record.seq for record in SeqIO.parse(handle, "fasta")]
-
-    predicted_exons = []
-
-    for n, seq in enumerate(seqs):
-        sequence = ""
-        exon_length = 0
-
-        for i in range(0, 3):
-            exon = seq[i:].translate(to_stop=True)
-
-            if len(exon) > exon_length:
-                sequence = exon
-                exon_length = len(exon)
-
-        predicted_exons.append(sequence)
-
-    print("".join([str(seq) for seq in predicted_exons]))
 
 
 if __name__ == "__main__":
