7acaeb1ef888d1ec4f74934853a2c62d73c73e73
powellor <oliver.powell@kaust.edu.sa>
2024-01-30 15:14:00 +0300

    ruff formatting

--- src/assemble_gene.py
+++ src/assemble_gene.py
@@ -5,9 +5,9 @@ import sys
 import os
 import warnings
 from Bio import SeqIO
-from Bio.Seq import Seq
 
 warnings.filterwarnings("ignore")
+
 
 class GeneAssembler:
     def __init__(self, BED_FILE, BLASTDB_PATH, EXON_LIST) -> None:
@@ -31,8 +31,10 @@ class GeneAssembler:
         self.exon_list: list[str] = EXON_LIST
 
         self.exon_data = self.bed[self.bed.name.isin(self.exon_list)]
-        
-        self.exon_data["name"] = pd.Categorical(self.exon_data["name"], categories=self.exon_list, ordered=True)
+
+        self.exon_data["name"] = pd.Categorical(
+            self.exon_data["name"], categories=self.exon_list, ordered=True
+        )
         self.exon_data.sort_values("name", inplace=True)
 
         self.blastdb: str = BLASTDB_PATH
@@ -72,27 +74,41 @@ class GeneAssembler:
             sys.exit(1)
 
     def load_ORFS_into_dataframe(self) -> None:
-
         if len(self.exon_data.strand.unique()) != 1:
             self.nuke()
             print("Error: all exon strands must be identical!")
             sys.exit(1)
 
         with open(self.exon_sequence_file.name) as handle:
-            self.exon_data["sequence"] = [record.seq for record in SeqIO.parse(handle, "fasta")]
+            self.exon_data["sequence"] = [
+                record.seq for record in SeqIO.parse(handle, "fasta")
+            ]
 
             if self.exon_data.strand.unique()[0] == "-":
-                self.exon_data.sequence = self.exon_data.sequence.map(lambda x: x.reverse_complement())
+                self.exon_data.sequence = self.exon_data.sequence.map(
+                    lambda x: x.reverse_complement()
+                )
 
-        self.exon_data["ORF1"] = self.exon_data.sequence.map(lambda x: x if len(x) % 3 == 0 else x[:-(len(x) % 3)])
-        self.exon_data["ORF2"] = self.exon_data.sequence.map(lambda x: x[1:] if len(x[1:]) % 3 == 0 else x[1:-(len(x[1:]) % 3)])
-        self.exon_data["ORF3"] = self.exon_data.sequence.map(lambda x: x[2:] if len(x[2:]) % 3 == 0 else x[2:-(len(x[2:]) % 3)])
-    
+        self.exon_data["ORF1"] = self.exon_data.sequence.map(
+            lambda x: x if len(x) % 3 == 0 else x[: -(len(x) % 3)]
+        )
+        self.exon_data["ORF2"] = self.exon_data.sequence.map(
+            lambda x: x[1:] if len(x[1:]) % 3 == 0 else x[1 : -(len(x[1:]) % 3)]
+        )
+        self.exon_data["ORF3"] = self.exon_data.sequence.map(
+            lambda x: x[2:] if len(x[2:]) % 3 == 0 else x[2 : -(len(x[2:]) % 3)]
+        )
+
     def translate_ORFS(self):
-        self.exon_data["prot1"] = self.exon_data.ORF1.map(lambda x: x.translate(to_stop=True))
-        self.exon_data["prot2"] = self.exon_data.ORF2.map(lambda x: x.translate(to_stop=True))
-        self.exon_data["prot3"] = self.exon_data.ORF3.map(lambda x: x.translate(to_stop=True))
-
+        self.exon_data["prot1"] = self.exon_data.ORF1.map(
+            lambda x: x.translate(to_stop=True)
+        )
+        self.exon_data["prot2"] = self.exon_data.ORF2.map(
+            lambda x: x.translate(to_stop=True)
+        )
+        self.exon_data["prot3"] = self.exon_data.ORF3.map(
+            lambda x: x.translate(to_stop=True)
+        )
 
     def predict_protein(self):
         protein = []
@@ -104,10 +120,10 @@ class GeneAssembler:
             prot_len = list(map(len, exon_prots))
 
             biggest_exon = prot_len.index(max(prot_len))
-            
+
             protein.append(exon_prots[biggest_exon])
             cds.append(exon_cds[biggest_exon])
-        
+
         self.protein = "".join([str(seq) for seq in protein])
         self.cds = "".join([str(seq) for seq in cds])
 
@@ -133,7 +149,6 @@ class GeneAssembler:
         print("Temporary files deleted...")
 
     def run(self) -> None:
-
         self.extract_exon_sequences()
         self.load_ORFS_into_dataframe()
         self.translate_ORFS()
@@ -148,7 +163,19 @@ if __name__ == "__main__":
     NLR_BED_FILE = "/home/powellor/Documents/projects/sr62_homeologues/scripts/SABAT_pipeline/SABAT/src/longissima_nlr_cds_refined.mega.bed"
     KINASE_BED_FILE = "/home/powellor/Documents/projects/sr62_homeologues/scripts/SABAT_pipeline/SABAT/src/longissima_sr62_cds_refined.mega.bed"
     NLR_EXON_LIST = ["exon_42", "exon_43", "exon_44"]
-    KINASE_EXON_LIST = ["exon_11", "exon_12", "exon_13", "exon_14", "exon_15", "exon_16", "exon_17", "exon_18", "exon_19", "exon_20", "exon_21"]
+    KINASE_EXON_LIST = [
+        "exon_11",
+        "exon_12",
+        "exon_13",
+        "exon_14",
+        "exon_15",
+        "exon_16",
+        "exon_17",
+        "exon_18",
+        "exon_19",
+        "exon_20",
+        "exon_21",
+    ]
     KINASE_EXON_LIST = KINASE_EXON_LIST[::-1]
     print(KINASE_EXON_LIST)
 
@@ -158,9 +185,10 @@ if __name__ == "__main__":
     )
     NLR_gene.run()
 
-
     print("\nAnalysing Kinase\n")
     KINASE_gene = GeneAssembler(
-        BED_FILE=KINASE_BED_FILE, BLASTDB_PATH=LONG_BLASTDB_PATH, EXON_LIST=KINASE_EXON_LIST
+        BED_FILE=KINASE_BED_FILE,
+        BLASTDB_PATH=LONG_BLASTDB_PATH,
+        EXON_LIST=KINASE_EXON_LIST,
     )
     KINASE_gene.run()
