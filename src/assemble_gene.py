55e06ccd1482730ae9c14ef602c6a3d1afc95ed7
powellor <oliver.powell@kaust.edu.sa>
2024-01-30 14:51:27 +0300

    Fully functional GeneAssembler prototype complete!
    Added the following:
    
    method: Load_ORFS_into_dataframe()
    - Now checks all sequences are on the same strand - quits if no
    - sequences are stored as Seq objects not strings for ease of translation/reverse complementation
    - if strand is reverse ("-") the reverse complement of all sequences are created
    
    method: translate_ORFS:
    - New!
    - Translates each ORF into a protein sequence (until first stop codon)
    
    method: predict_protein:
    - complete overhaul
    - still predicts the protein
    - Now also correctly predicts CDS (thanks to sequence trimming in Load_ORFS)
    - TBA: start/stop codon checker on first/last exon
    - TBA: write output to file
    
    General TBA: Adding logging

--- src/assemble_gene.py
+++ src/assemble_gene.py
@@ -5,6 +5,7 @@ import sys
 import os
 import warnings
 from Bio import SeqIO
+from Bio.Seq import Seq
 
 warnings.filterwarnings("ignore")
 
@@ -30,6 +31,9 @@ class GeneAssembler:
         self.exon_list: list[str] = EXON_LIST
 
         self.exon_data = self.bed[self.bed.name.isin(self.exon_list)]
+        
+        self.exon_data["name"] = pd.Categorical(self.exon_data["name"], categories=self.exon_list, ordered=True)
+        self.exon_data.sort_values("name", inplace=True)
 
         self.blastdb: str = BLASTDB_PATH
 
@@ -67,43 +71,45 @@ class GeneAssembler:
 
     def load_ORFS_into_dataframe(self) -> None:
 
+        if len(self.exon_data.strand.unique()) != 1:
+            self.nuke()
+            print("Error: all exon strands must be identical!")
+            sys.exit(1)
+
         with open(self.exon_sequence_file.name) as handle:
-            self.exon_data["sequence"] = [str(record.seq) for record in SeqIO.parse(handle, "fasta")]
+            self.exon_data["sequence"] = [record.seq for record in SeqIO.parse(handle, "fasta")]
+
+            if self.exon_data.strand.unique()[0] == "-":
+                self.exon_data.sequence = self.exon_data.sequence.map(lambda x: x.reverse_complement())
 
         self.exon_data["ORF1"] = self.exon_data.sequence.map(lambda x: x if len(x) % 3 == 0 else x[:-(len(x) % 3)])
         self.exon_data["ORF2"] = self.exon_data.sequence.map(lambda x: x[1:] if len(x[1:]) % 3 == 0 else x[1:-(len(x[1:]) % 3)])
         self.exon_data["ORF3"] = self.exon_data.sequence.map(lambda x: x[2:] if len(x[2:]) % 3 == 0 else x[2:-(len(x[2:]) % 3)])
-
-        print(self.exon_data)
-
-
-    def predict_protein(self) -> None:
-
-        print(f"Analysing gene containing {len(self.exon_data)} exon(s)\n")
-
-        with open(self.exon_sequence_file.name) as handle:
-            seqs = [record.seq for record in SeqIO.parse(handle, "fasta")]
-
-        predicted_exons = []
-
-        for n, seq in enumerate(seqs):
-            sequence = ""
-            exon_length = 0
-
-            for i in range(0, 3):
-                exon = seq[i:].translate(to_stop=True)
-
-                if len(exon) > exon_length:
-                    sequence = exon
-                    exon_length = len(exon)
-
-            predicted_exons.append(sequence)
-
-        self.protein = "".join([str(seq) for seq in predicted_exons])
-
-        print(self.protein)
+    
+    def translate_ORFS(self):
+        self.exon_data["prot1"] = self.exon_data.ORF1.map(lambda x: x.translate(to_stop=True))
+        self.exon_data["prot2"] = self.exon_data.ORF2.map(lambda x: x.translate(to_stop=True))
+        self.exon_data["prot3"] = self.exon_data.ORF3.map(lambda x: x.translate(to_stop=True))
 
 
+    def predict_protein(self):
+        protein = []
+        cds = []
+
+        for index, exon in self.exon_data.iterrows():
+            exon_cds = [exon.ORF1, exon.ORF2, exon.ORF3]
+            exon_prots = [exon.prot1, exon.prot2, exon.prot3]
+            prot_len = list(map(len, exon_prots))
+
+            print(prot_len)
+
+            biggest_exon = prot_len.index(max(prot_len))
+            
+            protein.append(exon_prots[biggest_exon])
+            cds.append(exon_cds[biggest_exon])
+        
+        print("".join([str(seq) for seq in protein]), "\n")
+        print("".join([str(seq) for seq in cds]))
 
     def generate_statistics(self) -> None:
         print(f"\nPredicted coverage: {self.exon_data.score.sum()}")
@@ -111,8 +117,16 @@ class GeneAssembler:
         print("CDS gene length: TBA")
 
     def nuke(self) -> None:
-        os.remove(self.batch_entry_file.name)
-        os.remove(self.exon_sequence_file.name)
+        try:
+            os.remove(self.batch_entry_file.name)
+        except FileNotFoundError:
+            print("batch file not found")
+
+        try:
+            os.remove(self.exon_sequence_file.name)
+        except FileNotFoundError:
+            print("sequences file not found")
+
         print("Temporary files deleted...")
 
     def run(self) -> None:
@@ -120,6 +134,8 @@ class GeneAssembler:
         self.extract_exon_sequences()
         #self.predict_protein()
         self.load_ORFS_into_dataframe()
+        self.translate_ORFS()
+        self.better_predict_protein()
         self.generate_statistics()
         self.nuke()
 
@@ -131,6 +147,8 @@ if __name__ == "__main__":
     KINASE_BED_FILE = "/home/powellor/Documents/projects/sr62_homeologues/scripts/SABAT_pipeline/SABAT/src/longissima_sr62_cds_refined.mega.bed"
     NLR_EXON_LIST = ["exon_42", "exon_43", "exon_44"]
     KINASE_EXON_LIST = ["exon_11", "exon_12", "exon_13", "exon_14", "exon_15", "exon_16", "exon_17", "exon_18", "exon_19", "exon_20", "exon_21"]
+    KINASE_EXON_LIST = KINASE_EXON_LIST[::-1]
+    print(KINASE_EXON_LIST)
 
     print("\nAnalysing NLR\n")
     NLR_gene = GeneAssembler(
@@ -138,10 +156,9 @@ if __name__ == "__main__":
     )
     NLR_gene.run()
 
-    """
+
     print("\nAnalysing Kinase\n")
     KINASE_gene = GeneAssembler(
         BED_FILE=KINASE_BED_FILE, BLASTDB_PATH=LONG_BLASTDB_PATH, EXON_LIST=KINASE_EXON_LIST
     )
     KINASE_gene.run()
-    """
