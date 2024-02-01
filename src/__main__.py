4f08a273fdbdedaf9b6580e5dd9fa77457176bf4
powellor <oliver.powell@kaust.edu.sa>
2024-02-01 16:22:15 +0300

    Added CLI command for assemble_gene to main

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,6 @@
 import click
 from blast_2_bed import blast2bed
-from assemble_gene import GeneAssembler
+from assemble_gene import assemble_gene
 
 @click.group(
     "S.A.B.A.T: Semi-Automatic BLAST Annotation Toolkit"
@@ -9,6 +9,7 @@ def cli() -> None:
     pass
 
 cli.add_command(blast2bed)
+cli.add_command(assemble_gene)
 
 if __name__ == "__main__":
     cli()
