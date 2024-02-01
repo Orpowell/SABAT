fcd797b14a27971594bb5582028c76ae7f23fdea
powellor <oliver.powell@kaust.edu.sa>
2024-02-01 13:51:10 +0300

    Removed function based implementation and replaced with OOP approach:
    The 2 functions were divided into 4 class methods:
    
    1. process_BLAST: handles pre-processing the BLAST file (strands, sorting sstart/send etc.)
    2. convert_BLAST_to_BED: converts BLAST into BED after processing
    3. predict_gene_loci: predicts gene loci
    4. write_bed_file: writes output
    
    A 5th method, run, was added to run all steps in the correct order

--- src/blast_2_bed.py
+++ src/blast_2_bed.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
-
-def BLAST2BED9(
-    input: str,
-    output: str,
-    locus_size: int,
-    exon_count: int,
-    q_cov_threshold: float,
-    refseq=False,
-) -> None:
-    cds_blast_data = pd.read_csv(
+class BLAST2BED:
+    def __init__(self,
+                 input: str,
+                 output: str,
+                 locus_size: int,
+                 exon_count: int,
+                 q_cov_threshold: float,
+                 refseq=False
+                ) -> None:
+    
+        self.cds_blast_data = pd.read_csv(
         input,
         sep="\t",
         header=None,
@@ -27,122 +28,115 @@ def BLAST2BED9(
             "evalue",
             "bitscore",
             "qlen",
-        ],
-    )
+            ],
+        )
+        self.bed9 = pd.DataFrame()
+        self.output = output
+        self.locus_size = locus_size
+        self.exon_count = exon_count
+        self.q_cov_threshold = q_cov_threshold
+        self.refseq = False
+    
+    def process_BLAST(self):
+            # Establish strand orientation and query coverage of BLAST hits
+        self.cds_blast_data["orientation"] = self.cds_blast_data.sstart < self.cds_blast_data.send
+        self.cds_blast_data["strand"] = self.cds_blast_data.orientation.map(
+            lambda x: "+" if x is True else "-"
+        )
+        self.cds_blast_data["qcov"] = round(self.cds_blast_data.length / self.cds_blast_data.qlen, 2)
 
-    # Establish strand orientation and query coverage of BLAST hits
-    cds_blast_data["orientation"] = cds_blast_data.sstart < cds_blast_data.send
-    cds_blast_data["strand"] = cds_blast_data.orientation.map(
-        lambda x: "+" if x is True else "-"
-    )
-    cds_blast_data["qcov"] = round(cds_blast_data.length / cds_blast_data.qlen, 2)
+        # Correctly order start/end of BLAST hits for BED
+        cond = self.cds_blast_data.sstart > self.cds_blast_data.send
+        self.cds_blast_data.loc[cond, ["sstart", "send"]] = self.cds_blast_data.loc[
+            cond, ["send", "sstart"]
+        ].values
 
-    # Correctly order start/end of BLAST hits for BED
-    cond = cds_blast_data.sstart > cds_blast_data.send
-    cds_blast_data.loc[cond, ["sstart", "send"]] = cds_blast_data.loc[
-        cond, ["send", "sstart"]
-    ].values
+        # Sort values + reindex
+        self.cds_blast_data.sort_values(by="sstart", inplace=True)
+        self.cds_blast_data.reset_index(drop=True, inplace=True)
 
-    # Sort values + reindex
-    cds_blast_data.sort_values(by="sstart", inplace=True)
-    cds_blast_data.reset_index(drop=True, inplace=True)
+        # Remove any formatting from BLASTDB from sequence IDs
+        if self.refseq:
+            self.cds_blast_data["chromosome"]: str = self.cds_blast_data.sseqid.map(  # type: ignore
+                lambda x: x.split("|")[1]
+            )
 
-    # Initialise new dataframe for bed file
-    bed9 = pd.DataFrame()
+        else:
+            self.cds_blast_data["chromosome"] = self.cds_blast_data.sseqid
 
-    # Remove any formatting from BLASTDB from sequence IDs
-    if refseq:
-        cds_blast_data["chromosome"]: str = cds_blast_data.sseqid.map(  # type: ignore
-            lambda x: x.split("|")[1]
+    def convert_BLAST_to_BED(self):
+        # Fill columns 1-9
+        self.bed9["chrom"]: str = self.cds_blast_data.chromosome  # type: ignore
+        self.bed9["chromStart"]: int = self.cds_blast_data.sstart  # type: ignore
+        self.bed9["chromEnd"]: int = self.cds_blast_data.send  # type: ignore
+        self.bed9["name"]: str = [f"exon_{i}" for i in self.cds_blast_data.index]  # type: ignore
+        self.bed9["score"]: float = self.cds_blast_data.qcov  # type: ignore
+        self.bed9["strand"]: str = self.cds_blast_data.strand  # type: ignore
+        self.bed9["thickStart"]: int = self.cds_blast_data.sstart  # type: ignore
+        self.bed9["thickEnd"]: int = self.cds_blast_data.send  # type: ignore
+        self.bed9["itemRgb"]: str = "145,30,180"  # type: ignore
+
+    def predict_gene_loci(self):
+
+        gene_locus = 0
+
+        for window in self.cds_blast_data.sort_values(["chromosome", "strand"]).rolling(
+            self.exon_count
+        ):
+            if (
+                len(window) > self.exon_count - 1
+                and all(window.chromosome.unique())  # type: ignore
+                and all(window.strand.unique())
+            ):
+                locus_qcov = round(window.qcov.sum(), 2)
+                size = window.send.max() - window.sstart.min()
+
+                if locus_qcov > self.q_cov_threshold and size < self.locus_size * 1.5:
+                    self.bed9.loc[len(self.bed9.index)] = [
+                        window.chromosome.unique()[0],
+                        window.sstart.min(),
+                        window.send.max(),
+                        f"locus_{gene_locus}",
+                        locus_qcov,
+                        window.strand.unique()[0],
+                        window.sstart.min(),
+                        window.send.max(),
+                        "0,255,0",
+                    ]
+                    gene_locus += 1
+    
+    def write_bed_file(self):
+            # Save output to bed file (formatted as tsv)
+        self.bed9.to_csv(
+            self.output,
+            sep="\t",
+            header=False,
+            index=False,
+            columns=[
+                "chrom",
+                "chromStart",
+                "chromEnd",
+                "name",
+                "score",
+                "strand",
+                "thickStart",
+                "thickEnd",
+                "itemRgb",
+            ],
         )
 
-    else:
-        cds_blast_data["chromosome"] = cds_blast_data.sseqid
-
-    # Fill columns 2-9
-    bed9["chrom"]: str = cds_blast_data.chromosome  # type: ignore
-    bed9["chromStart"]: int = cds_blast_data.sstart  # type: ignore
-    bed9["chromEnd"]: int = cds_blast_data.send  # type: ignore
-    bed9["name"]: str = [f"exon_{i}" for i in cds_blast_data.index]  # type: ignore
-    bed9["score"]: float = cds_blast_data.qcov  # type: ignore
-    bed9["strand"]: str = cds_blast_data.strand  # type: ignore
-    bed9["thickStart"]: int = cds_blast_data.sstart  # type: ignore
-    bed9["thickEnd"]: int = cds_blast_data.send  # type: ignore
-    bed9["itemRgb"]: str = "145,30,180"  # type: ignore
-
-    # Label gene loci according to parameters and add to bed file
-    # This provides confidence in the absence of annotations
-    gene_locus = 0
-
-    for window in cds_blast_data.sort_values(["chromosome", "strand"]).rolling(
-        exon_count
-    ):
-        if (
-            len(window) > exon_count - 1
-            and all(window.chromosome.unique())  # type: ignore
-            and all(window.strand.unique())
-        ):
-            locus_qcov = round(window.qcov.sum(), 2)
-            size = window.send.max() - window.sstart.min()
-
-            if locus_qcov > q_cov_threshold and size < locus_size * 1.5:
-                bed9.loc[len(bed9.index)] = [
-                    window.chromosome.unique()[0],
-                    window.sstart.min(),
-                    window.send.max(),
-                    f"locus_{gene_locus}",
-                    locus_qcov,
-                    window.strand.unique()[0],
-                    window.sstart.min(),
-                    window.send.max(),
-                    "0,255,0",
-                ]
-                gene_locus += 1
-
-    # Save output to bed file (formatted as tsv)
-    bed9.to_csv(
-        output,
-        sep="\t",
-        header=False,
-        index=False,
-        columns=[
-            "chrom",
-            "chromStart",
-            "chromEnd",
-            "name",
-            "score",
-            "strand",
-            "thickStart",
-            "thickEnd",
-            "itemRgb",
-        ],
-    )
-
+    def run(self):
+        self.process_BLAST()
+        self.convert_BLAST_to_BED()
+        self.predict_gene_loci()
+        self.write_bed_file()
 
 if __name__ == "__main__":
-    NLR_EXONS = 3
-    NLR_LOCUS = 6000
-    NLR_QCOV = 0.8
 
-    KINASE_EXONS = 9
-    KINASE_LOCUS = 10000
-    KINASE_QCOV = 0.8
-
-    BLAST2BED9(
-        "/home/powellor/Documents/projects/sr62_homeologues/analysis/blast_annotation_mapping/test_data/TA10171/Sr62_cds.blastn.mega",
-        "TA10171_sr62.mega.bed",
-        exon_count=KINASE_EXONS,
-        q_cov_threshold=KINASE_QCOV,
-        locus_size=KINASE_LOCUS,
-    )
-
-    BLAST2BED9(
-        "/home/powellor/Documents/projects/sr62_homeologues/analysis/blast_annotation_mapping/test_data/sharonensis/sh_Sr62_cds.blastn.mega",
-        "sh_sr62.mega.bed",
-        exon_count=KINASE_EXONS,
-        q_cov_threshold=KINASE_QCOV,
-        locus_size=KINASE_LOCUS,
-    )
-
-
-
+    vis = BLAST2BED(input= "/home/powellor/Documents/projects/sr62_homeologues/analysis/blast_annotation_mapping/test_data/sharonensis/sh_Sr62_cds.blastn.mega",
+              exon_count= 9,
+              q_cov_threshold= 0.8,
+              locus_size = 10000,
+              output= "c2b_test_sh_sr62.mega.bed"
+            )
+    vis.run()
