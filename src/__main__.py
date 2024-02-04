88003e747adda4a8279d69c6c802fccae96927f6
powellor <oliver.powell@kaust.edu.sa>
2024-02-04 11:36:54 +0300

    formatting

--- src/__main__.py
+++ src/__main__.py
@@ -4,8 +4,13 @@ import click
 from blast_2_bed import blast2bed
 from assemble_gene import assemble_gene
 
-logging.basicConfig(stream=sys.stderr, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
-                    level=logging.INFO)
+logging.basicConfig(
+    stream=sys.stderr,
+    format="%(asctime)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    level=logging.INFO,
+)
+
 
 @click.group(help="S.A.B.A.T: Semi-Automatic BLAST Annotation Toolkit")
 def cli() -> None:
