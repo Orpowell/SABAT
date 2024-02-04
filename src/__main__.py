e58751ab97aa75bf351524aefe1783d1653f1c0f
powellor <oliver.powell@kaust.edu.sa>
2024-02-04 11:23:07 +0300

    Added stderr logging config

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,13 @@
+import logging
+import sys
 import click
 from blast_2_bed import blast2bed
 from assemble_gene import assemble_gene
 
+logging.basicConfig(stream=sys.stderr, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
+                    level=logging.INFO)
 
-@click.group("S.A.B.A.T: Semi-Automatic BLAST Annotation Toolkit")
+@click.group(help="S.A.B.A.T: Semi-Automatic BLAST Annotation Toolkit")
 def cli() -> None:
     pass
 
