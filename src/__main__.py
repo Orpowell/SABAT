323fbaaa20b5d41f2e18f8fbbaf439fa15ef454e
powellor <oliver.powell@kaust.edu.sa>
2024-02-19 18:04:40 +0300

    Added basic subparsers for each command in argparse

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 import click
+import argparse
 from blast_2_bed import blast2bed
 from assemble_gene import assemble_exons
 from assemble_gene import assemble_locus
@@ -18,10 +19,30 @@ logging.basicConfig(
 def cli() -> None:
     pass
 
-
 cli.add_command(blast2bed)
 cli.add_command(assemble_exons)
 cli.add_command(assemble_locus)
 
+def main():
+    parser = argparse.ArgumentParser()
+
+    sub_parsers = parser.add_subparsers(dest="command")
+
+    blast2bed_parser = sub_parsers.add_parser("blast2bed", help="Convert BLAST output to BED and predict gene loci")
+
+
+    exon_parser = sub_parsers.add_parser("assemble-exons", help="Assemble gene from specified exons")
+    exon_parser.add_argument("-i", metavar="--input", help="path to bed file")
+
+    locus_parser = sub_parsers.add_parser("assemble-locus", help="Assemble gene from a defined locus")
+
+    args = parser.parse_args()
+
+    print(args)
+
+
+
+
 if __name__ == "__main__":
-    cli()
+    main()
+    #cli()
